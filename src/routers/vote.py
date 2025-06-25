10ba8a7d0fad8fc1423ac9be6e07a97c64a78ddb
shankarganesh05 <shankarganesh.d@gmail.com>
2025-06-25 07:38:36 +0530

    Fixed Bug in Vote

--- src/routers/vote.py
+++ src/routers/vote.py
@@ -8,18 +8,21 @@ router = APIRouter(prefix="/vote",tags=["vote"])
 
 @router.post("/",status_code=status.HTTP_201_CREATED)
 def create_vote(vote:schemas.Vote,db:Session = Depends(get_session),current_user:int = Depends(outh2.get_current_user)):
+    post = db.get(model.Post,vote.post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {vote.post_id} does not exist")
     vote_query = db.exec(select(model.Vote).filter(model.Vote.post_id==vote.post_id,model.Vote.user_id==current_user.id))
     found_vote = vote_query.first()
     if vote.dir == True:
         if found_vote:
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT,details=f"Vote for post {vote.post_id} already exist")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"Vote for post {vote.post_id} already exist")
         new_vote = model.Vote(post_id = vote.post_id,user_id=current_user.id)
         db.add(new_vote)
         db.commit()
         return {"message":"Successfully added your vote"}
     else:
         if not found_vote:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,details="Vote doesn't exist")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Vote doesn't exist")
         db.delete(found_vote)
         db.commit()
         return {"message": "Successfully deleted your vote"}
