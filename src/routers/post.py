01c5c8d41fcc4576562d7abd4bbda39e2f6a8062
shankarganesh05 <shankarganesh.d@gmail.com>
2025-06-25 20:47:28 +0530

    Modified the Get Post to display votes

--- src/routers/post.py
+++ src/routers/post.py
@@ -9,18 +9,20 @@ router = APIRouter(prefix="/posts",tags=['Posts'])
 # Getting all posts
 @router.get("/",response_model = List[schemas.Postout])
 def get_posts(db: Session = Depends(get_session),current_user:int=Depends(outh2.get_current_user),limit:int=10,skip:int=0,search:str=""):
-    posts = db.exec(select(model.Post)).all()
+    #posts = db.exec(select(model.Post)).all()
     result = db.exec(select(model.Post,func.count(model.Vote.post_id).label("Votes")).join(model.Vote,model.Post.id==model.Vote.post_id,isouter=True).group_by(model.Post.id).limit(limit).offset(skip).filter(model.Post.title.contains(search))).all()
     return [{"Post": post, "votes": votes} for post, votes in result]
   
 # Getting a single post by id
-@router.get("/{id}",response_model=schemas.PostResponse)
+@router.get("/{id}",response_model=List[schemas.Postout])
 def get_post(id: int, db: Session = Depends(get_session),current_user:int=Depends(outh2.get_current_user)):
-    post = db.get(model.Post, id)
-    if post:
-        if post.user_id!= current_user.id:
+    post_query = db.get(model.Post, id)
+    result = db.exec(select(model.Post,func.count(model.Vote.post_id).label("Votes")).join(model.Vote,model.Post.id==model.Vote.post_id,isouter=True).group_by(model.Post.id).filter(model.Post.id == id)).all()
+    print(result)
+    if post_query:
+        if post_query.user_id!= current_user.id:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Not Authorized to perform the action")
-        return post
+        return [{"Post": post, "votes": votes} for post, votes in result]
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} does not exist")
     
 # Creating a new post 
